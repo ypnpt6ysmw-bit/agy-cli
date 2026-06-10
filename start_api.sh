0c3bd40d6cd101b040af1182b201e2991bb4553a
Ariel Kurek <arielkurek@aribook-pro-1.home>
2026-06-10 16:45:58 +0200

    Sync repository scripts with working deployment versions

--- start_api.sh
+++ start_api.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cd /Users/arielkurek/Desktop/Ari/AGY
+cd /Users/arielkurek/.hermes/agy-api
 export PATH="/Users/arielkurek/.local/bin:/opt/homebrew/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
 export HOME="/Users/arielkurek"
+export PYTHONUNBUFFERED=1
 exec .venv/bin/python -u agy_api.py
